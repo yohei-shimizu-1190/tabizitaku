fed8c0b864f174ff404eb3626de79faa65bf0c2f
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-06 17:26:26 +0900

    savepoint1(spots/index.html) #2

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,6 +1,10 @@
 class SpotsController < ApplicationController
   def index
-    
+    @spots = Spot.all
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
   end
 
   def jmap
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "top#index"
-  resources :spots, only: :index do
+  resources :spots, only: [:index, :show] do
     collection do
       get 'jmap'
     end
