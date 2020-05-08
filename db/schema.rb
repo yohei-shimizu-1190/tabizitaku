9a6680c85c94afc82c8a5b9b39e57c162811c429
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-08 09:43:20 +0900

    feat(spots/show.html):GoogleMapAPI導入 #2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_120931) do
+ActiveRecord::Schema.define(version: 2020_05_07_065609) do
 
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_120931) do
     t.string "region", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
