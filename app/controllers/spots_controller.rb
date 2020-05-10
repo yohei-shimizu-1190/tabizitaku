2afa13058ba077af6284f73db92bae9c53023bf0
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-10 17:13:19 +0900

    feat(spot/imap.html):一覧マップページ追加 #8

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,11 +2,11 @@ class SpotsController < ApplicationController
   def index
     @spots = Spot.all
   end
-
+  
   def show
     @spot = Spot.find(params[:id])
   end
-
+  
   def quiz
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_065609) do
+ActiveRecord::Schema.define(version: 2020_05_10_040407) do
 
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,23 @@ ActiveRecord::Schema.define(version: 2020_05_07_065609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.integer "status"
+    t.float "latitude"
+    t.float "longitude"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
