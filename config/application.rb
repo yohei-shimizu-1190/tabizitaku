dfde3052b717d72bd419228516bcd46c11df1e52
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-18 22:26:59 +0900

    deploy(asset.pipeline)

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Rails.application.config.assets.precompile += %w( *.eot *.woff *.ttf *.svg *.otf *.png *.jpg *.jpeg *.gif vendor.css vendor.js )
 module Tabizitaku
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
